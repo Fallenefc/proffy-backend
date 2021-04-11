f020ceb3941937b13f3c29655767a8df9a9f1382
wenked <wenked.matheus@yahoo.com.br>
2021-04-11 13:04:26 -0300

    boilimax

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
-import express from 'express'
-import cors from 'cors'
-import routes from "./routes"
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+console.log('boilimax');
 
 app.listen(3333);
